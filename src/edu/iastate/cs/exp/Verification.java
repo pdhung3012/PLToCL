f6e777653c92d45b926c3622f893bba27f053438
pdhung3012 <pdhung3012@gmail.com>
2017-12-30 14:05:09 -0600

    add verification

--- src/edu/iastate/cs/exp/Verification.java
+++ src/edu/iastate/cs/exp/Verification.java
@@ -40,8 +40,8 @@ public class Verification {
 		System.out.println("set grammar: "+setTrainedGrammar.size());
 
 		int numCountInCorrect = 0;
-		int numForTest = 10000;
-		for (int i = 0; i < numForTest; i++) {
+		int numForTest = 1000;
+		for (int i = 0; i <numForTest; i++) {
 			boolean isValid = true;
 			//System.out.println(arrContentTest[i]);
 			Tree tree = gh.getTreeFromSentence(arrContentTest[i], lp,
@@ -66,10 +66,97 @@ public class Verification {
 			}
 
 		}
-		System.out.println("Verification results: " + numCountInCorrect + "/"
-				+ numForTest);
+		System.out.println("Verification results: " + (numForTest- numCountInCorrect) + "/"
+				+ numForTest+" = "+(numForTest- numCountInCorrect)*1.0/numForTest);
 
 	}
+	
+	public void calculateGrammar(String fileTrainingGrammar,String fileTest,String fileLabel,String fileDetails) {
+		String[] arrContentTrainGrammar = FileIO
+				.readStringFromFile(fileTrainingGrammar).trim().split("\n");
+		String[] arrContentTest = FileIO.readStringFromFile(fileTest).trim()
+				.split("\n");
+		String[] arrLabel = FileIO.readStringFromFile(fileLabel).trim()
+				.split("\n");
+
+		GrammarHeuristic gh = new GrammarHeuristic();
+		TokenizerFactory<CoreLabel> tokenizerFactory = PTBTokenizer.factory(
+				new CoreLabelTokenFactory(), "");
+		LexicalizedParser lp = LexicalizedParser
+				.loadModel(PathConstanct.PATH_DEFAULTPARSEMODEL);
+
+		HashSet<String> setTrainedGrammar = new HashSet<String>();
+		for (int i = 0; i < arrContentTrainGrammar.length; i++) {
+			String[] arrItem = arrContentTrainGrammar[i].split(":");
+			int numCount=Integer.parseInt(arrItem[2].trim());
+			if(numCount<=50){
+				continue;
+			}
+			String strItem = arrItem[0].trim() + " : " + arrItem[1].trim();
+			//System.out.println("Item: "+strItem);
+			setTrainedGrammar.add(strItem);
+
+		}
+		
+		System.out.println("set grammar: "+setTrainedGrammar.size());
+		int numTruePositive=0;
+		int numFalsePositive=0;
+		int numTrueNegative=0;
+		int numFalseNegative=0;
+
+		int numCountInCorrect = 0;
+		int numForTest = 1000;
+		StringBuilder sbDetails=new StringBuilder();
+		for (int i = 0; i <arrContentTest.length; i++) {
+			boolean isValid = true;
+			//System.out.println(arrContentTest[i]);
+			Tree tree = gh.getTreeFromSentence(arrContentTest[i], lp,
+					tokenizerFactory);
+			ArrayList<GrammarRule> lstRules = new ArrayList<GrammarRule>();
+			gh.getAllRules(tree, lstRules);
+
+			for (int j = 0; j < lstRules.size(); j++) {
+				GrammarRule ruleItem = lstRules.get(j);
+				if (!setTrainedGrammar.contains(ruleItem.print())) {
+					isValid = false;
+			//		System.out.println(ruleItem.print()+" not exist");
+					break;
+				}
+			}
+			
+			sbDetails.append(isValid+"\t"+arrLabel[i].trim()+"\n");
+
+			if (isValid&&arrLabel[i].equals("true")) {
+				numTruePositive++;
+//				numCountInCorrect++;
+//				System.out.println(i+1);
+			} else if(isValid&&arrLabel[i].equals("false")){
+				numFalsePositive++;
+			} else if(!isValid&&arrLabel[i].equals("false")){
+				numTrueNegative++;
+			} else if(!isValid&&arrLabel[i].equals("true")){
+				numFalseNegative++;
+			}
+
+		}
+		sbDetails.append("TP: "+numTruePositive+"\n");
+		sbDetails.append("FP: "+numFalsePositive+"\n");
+		sbDetails.append("TN: "+numTrueNegative+"\n");
+		sbDetails.append("FN: "+numFalseNegative+"\n");
+		sbDetails.append("Precision: "+numTruePositive*1.0/(numTruePositive+numFalsePositive)+"\n");
+		sbDetails.append("Recall: "+numTruePositive*1.0/(numTruePositive+numFalseNegative)+"\n");
+		
+		System.out.println("Verification results: ");
+		System.out.println("TP: "+numTruePositive);
+		System.out.println("FP: "+numFalsePositive);
+		System.out.println("TN: "+numTrueNegative);
+		System.out.println("FN: "+numFalseNegative);
+		System.out.println("Precision: "+numTruePositive*1.0/(numTruePositive+numFalsePositive));
+		System.out.println("Recall: "+numTruePositive*1.0/(numTruePositive+numFalseNegative));
+		FileIO.writeStringToFile(sbDetails.toString(), fileDetails);
+
+	}
+
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
--- src/edu/iastate/cs/run/RunVerify.java
+++ src/edu/iastate/cs/run/RunVerify.java
@@ -10,9 +10,11 @@ public class RunVerify {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Verification ver=new Verification();
-		String fileTrainingGrammar=PathConstanct.PATH_OUTPUTFOLDER+"apacheComments_v2"+File.separator+"setRules.txt";
-		String fileTest=PathConstanct.PATH_OUTPUTFOLDER+"eclipseComments"+File.separator+"comment.txt";
-		ver.calculateGrammar(fileTrainingGrammar, fileTest);
+		String fileTrainingGrammar=PathConstanct.PATH_OUTPUTFOLDER+"apacheComments_v3_400K"+File.separator+"setRules.txt";
+		String fileTest=PathConstanct.PATH_OUTPUTFOLDER+"cliffer_grammars"+File.separator+"test.txt";
+		String fileDetails=PathConstanct.PATH_OUTPUTFOLDER+"cliffer_grammars"+File.separator+"detailResults.txt";
+		String fileLabel=PathConstanct.PATH_OUTPUTFOLDER+"cliffer_grammars"+File.separator+"label.txt";
+		ver.calculateGrammar(fileTrainingGrammar, fileTest,fileLabel,fileDetails);
 	}
 
 }
